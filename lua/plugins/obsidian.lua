2f6a7e748b0806b1826ee83921a90665a6aba5a3
floatingman <dan@danlovesprogramming.com>
2024-11-05 15:28:52 -0600

    Added obsidian plugin and updated plugins

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  -- ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre ~/SynologyDrive/vaults/personal/*.md",
+    "BufNewFile ~/SynologyDrive/vaults/personal/*.md",
+    "BufReadPre ~/SynologyDrive/vaults/work/*.md",
+    "BufNewFile ~/SynologyDrive/vaults/work/*.md",
+  },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/SynologyDrive/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/SynologyDrive/vaults/work",
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
