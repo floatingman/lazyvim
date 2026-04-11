59d4bd35867c5c34aae86359b0c632f57930bcab
floatingman <dan@danlovesprogramming.com>
2026-04-11 11:24:10 -0500

    Use terminal theme

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Load theme settings from toggle-theme.sh
+pcall(dofile, vim.fn.stdpath("config") .. "/lua/config/theme.lua")
